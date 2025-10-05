0306eee8b74b94b8f86bb9e8bdd43a0114aa074d
Kakeru Fukuda <stand.by.we@gmail.com>
2025-10-05 17:38:07 +0900

    ガワ作成

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by kakeru on 2025/10/05.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
